2e8ab6892c72028cf4bf8b408bb6aa7a9532453f
Joshua <yoshiserry@gmail.com>
2018-03-16 18:47:51 +1100

    updated script to prompt for password once at the start and make additional osx customisations to Finder.

--- README.md
+++ README.md
@@ -13,3 +13,9 @@
 * **To Do**
  - incorporate font installation and zsh theme for iterm2 colbat
  - add additional application packages using the bootstrap method.
+ - installation of ZSH themes --> https://github.com/wesbos/Cobalt2-iterm https://www.youtube.com/watch?v=XSeO6nnlWHw
+ - additional customisations:
+ - https://gist.github.com/MatthewMueller/e22d9840f9ea2fee4716
+ - https://news.ycombinator.com/item?id=8402079
+ - http://notes.jerzygangi.com/the-best-pgp-tutorial-for-mac-os-x-ever/
+ - alias command to reference it for "cleanup"
--- appinstall.sh
+++ appinstall.sh
@@ -1,22 +1,33 @@
 #!/usr/bin/env bash
 
+# Ask for the administrator password upfront
+sudo -v
+
 #set defaults for files in finder and dock settings
+echo "Setting finder defaults"
 defaults write com.apple.finder AppleShowAllFiles YES; # show hidden files
 defaults write com.apple.dock tilesize -int 36; # smaller icon sizes in Dock
 defaults write com.apple.dock autohide -bool true; # turn Dock auto-hidng on
 defaults write com.apple.dock autohide-delay -float 0; # remove Dock show delay
 defaults write com.apple.dock autohide-time-modifier -float 0; # remove Dock show delay
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true; # show all file extensions
-killall Dock 2>/dev/null;
-killall Finder 2>/dev/null;
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true; # Show filename extensions by default
+
+killall Dock 2>/dev/null; #kill dock
+killall Finder 2>/dev/null;#kill finder
+
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true; #expand save panel
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true; #expand save panel
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true; #expand save panel
+
+#echo "Never go into computer sleep mode"
+#systemsetup -setcomputersleep Off > /dev/null
 
 #install Xcode Command Line Tools
+echo "Install xcode command line tools from github"
 
-#curl -fsSL "https://raw.githubusercontent.com/timsutton/osx-vm-templates/master/scripts/xcode-cli-tools.sh" | pbcopy | pbpaste > /Users/joshua/Documents/xcodetools.sh;
 sh -c "($ curl -fsSL https://raw.githubusercontent.com/timsutton/osx-vm-templates/master/scripts/xcode-cli-tools.sh)";
-
 sh xcodetools.sh;
-
 touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
 
 PROD=$(softwareupdate -l |
@@ -26,42 +37,82 @@ PROD=$(softwareupdate -l |
   tr -d '\n')
   softwareupdate -i "$PROD" -v;
 
+  #install Homebrew
+  echo "Install Homebrew package manager for osx"
+
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 brew doctor && brew cask cleanup && brew update-reset && brew update;
-brew install git;
-brew install zsh zsh-completions;
+
+echo "prepare installation of homebrew apps..."
+
+brew tap homebrew/dupes;
+
+#coreutils
+#gnu-sed --with-default-names
+#gnu-tar --with-default-names
+#gnu-indent --with-default-names
+#gnu-which --with-default-names
+#gnu-grep --with-default-names
+
+BREW=(
+bash
+git
+zsh
+zsh-completions
+)
+
+echo "Installing brew apps..."
+brew install ${BREW[@]}
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
+
+#https://github.com/caskroom/homebrew-versions
+echo "installing support for alternate versions of Casks E.g. iterm2-beta"
 brew tap caskroom/versions;
 brew doctor && brew cask cleanup && brew update-reset && brew update;
-brew cask install atom;
-brew cask install keka;
-brew cask install keepassxc;
-brew cask install ccleaner;
-brew cask install caret;
-brew cask install mpv;
-brew cask install cyberduck;
-brew cask install dropbox;
-brew cask install firefox;
-brew cask install google-chrome;
-brew cask install spectacle;
-brew cask install vlc;
-brew cask install telegram;
-brew cask install evernote;
-brew cask install simplenote;
-brew cask install flux;
-brew cask install android-file-transfer;
-brew cask install androidtool;
-brew cask install teamviewer;
-brew cask install raindropio;
-brew cask install ticktick;
-brew cask install insomniax;
-brew cask install gitkraken
-brew cask install iterm2
-brew doctor && brew cask cleanup && brew update-reset && brew update;
-brew cask install anaconda
-chsh -s /usr/local/bin/zsh;
-echo "Installation of packages from Github using Homebrew and Cask complete!";
 
-#installation of ZSH themes
-#1. https://github.com/wesbos/Cobalt2-iterm https://www.youtube.com/watch?v=XSeO6nnlWHw
-#2. https://github.com/bhilburn/powerlevel9k
+echo "prepare installation of Cask apps..."
+
+CASKS=(
+atom
+keka
+keepassxc
+ccleaner
+caret
+mpv
+cyberduck
+dropbox
+firefox
+google-chrome
+spectacle
+vlc
+telegram
+evernote
+simplenote
+flux
+android-file-transfer
+androidtool
+teamviewer
+raindropio
+ticktick
+insomniax
+gitkraken
+iterm2
+)
+
+echo "Installing cask apps..."
+brew cask install ${CASKS[@]}
+
+echo "Cleaning up homebrew and cask..."
+brew doctor && brew cask cleanup && brew update-reset && brew update;
+
+echo "Installing anaconda..."
+brew cask install anaconda
+
+echo "Cleaning up homebrew and cask..."
+brew doctor && brew cask cleanup && brew update-reset && brew update;
+
+echo "Changing SHELL to Zsh..."
+chsh -s /usr/local/bin/zsh;
+
+echo "Bootstrap of OSX package installation from Github; Homebrew and Cask is now complete!"
